df3c494796db0d22f1b5d2dfa8632cc209f18a14
Igor Podsekin <wonderu@gmail.com>
2024-05-15 15:27:26 +0200

    Add docker installation

---
+++ install_docker.sh
@@ -0,0 +1,15 @@
+ 
+#!/bin/bash
+
+# Install Docker.io
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+sudo usermod -aG docker $USER
+
+# Install Docker Compose
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+# Verify installation
+docker --version
+docker-compose --version
